f8752fbfa00acdfb7e3fd961c27d49a7f55b6f7f
rafu01 <nrafsan13@gmail.com>
2024-04-27 13:00:01 +0600

    Add cors policy to handler

--- backend/src/handler.ts
+++ backend/src/handler.ts
@@ -7,7 +7,9 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+}));
 app.use(express.json());
 
 app.post("/project", async (request, response) => {
